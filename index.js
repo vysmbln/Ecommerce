963d50e01b92dde2e3094f2e0e4c37ccea79e0bc
vysmbln <vysmbln@users.noreply.github.com>
2020-04-01 12:17:00 -0400

    create form

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign up </button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/', (req, res)=> {
+    res.send('Account Created')
+})
+
+app.listen(3000, () =>{
+    console.log('listening')
+});
