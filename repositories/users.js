822edaa7a7059b3b8067cac71a6b8eaf7fb7f361
vysmbln <vysmbln@users.noreply.github.com>
2020-04-07 20:17:47 -0400

    update e,p,u

--- repositories/users.js
+++ repositories/users.js
@@ -38,16 +38,40 @@
     randomId(){
         return crypto.randomBytes(4).toString('hex');
     }
+
+    async getOne(id){
+        const records = await this.getAll();
+        return records.find(record => record.id === id);
+    }
+    async delete(id){
+        const records = await this.getAll();
+        const filteredRecords = records.filter(record => record.id != id);
+        await this.writeAll(filteredRecords)
+    }
+    async update(id, attrs){
+        const records = await this.getAll();
+        const record = records.find(record => record.id === id );
+
+        if (!record){
+            throw Error(`Record with id ${id} not found`);
+        }
+        Object.assign(record, attrs);
+        await this.writeAll(records)
+    }
+    async getOneBy(filters){
+        const records = await this.getAll();
+        for (let record of records){
+            let found = true;
+            for(let key in filters){
+                if(record[key] !== filters[key]){
+                    found = false;
+                }
+            }
+            if(found){
+                return record;
+            }
+        }
+    }
  }
 
-const test = async() => {
-    const repo = new UsersRepository('users.json');
-
-    repo.create({ email: 'test@test.com', password: 'password'});
-    
-    const users = await repo.getAll();
-
-    console.log(users);
-}
-
-test();
+ module.exports = new UsersRepository('user.json');
